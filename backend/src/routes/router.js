64307d6fe5dc61be00ae431092c6c76a503433d8
its-me-yps <yps8181@gmail.com>
2024-03-17 14:02:28 +0530

    student routes

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -4,12 +4,14 @@ import admin from '../controllers/admin.js'
 import student from '../controllers/student.js';
 import mailer from '../otp/mail.js'
 import jwtAuth from '../middlewares/auth.js';
+import pay from '../payment/pay.js';
 
 const router = express.Router();
 
 // Session Routes
 router.post('/session/admin/login', session.adminLogin);
-router.post('/session/user/login', session.userLogin);
+router.post('/session/student/login', session.studentLogin);
+router.post('/session/washerman/login', session.washermanLogin);
 router.get('/session/logout', session.logout);
 
 // Register Routes
@@ -22,5 +24,13 @@ router.use(jwtAuth);
 // Admin Routes
 router.post('/admin/washerman/register', admin.registerWasherman);
 router.post('/admin/addHallData', admin.addHallData);
+
+// Student Routes
+router.post('/student/fetchRecords', student.fetchRecord);
+router.post('/student/clearDue', student.clearDue);
+// For payment verification from RazorPay
+router.post('/verifyPayment', pay.verifyPayment);
+
+// Washerman routes
 
 export default router;
