132ec5f43b8980e1c151b6392c43ac97d90b290c
its-me-yps <yps8181@gmail.com>
2024-03-18 02:11:27 +0530

    routes addition

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -5,13 +5,17 @@ import student from '../controllers/student.js';
 import mailer from '../otp/mail.js'
 import jwtAuth from '../middlewares/auth.js';
 import pay from '../payment/pay.js';
+import washerman from '../controllers/washerman.js';
 
 const router = express.Router();
 
 // Session Routes
 router.post('/session/admin/login', session.adminLogin);
 router.post('/session/student/login', session.studentLogin);
+// washerman login also creates a web socket conn
 router.post('/session/washerman/login', session.washermanLogin);
+// washerman logout also removes web socket conn
+router.post('/session/washerman/logout', session.washermanLogout);
 router.get('/session/logout', session.logout);
 
 // Register Routes
@@ -26,11 +30,18 @@ router.post('/admin/washerman/register', admin.registerWasherman);
 router.post('/admin/addHallData', admin.addHallData);
 
 // Student Routes
-router.post('/student/fetchRecords', student.fetchRecord);
-router.post('/student/clearDue', student.clearDue);
+// requestWash is done through web socket conn and response is received through event listener
+router.post('/student/requestWash', student.requestWash);
+router.post('/student/fetchRecord', student.fetchRecord);
+router.get('/student/fetchDates', student.fetchDates);
+router.post('/student/payment/fetchReceipt', student.fetchReceipt);
+router.get('/student/payment/fetchDates', student.paymentDates);
+router.get('/student/payment/clearDue', student.clearDue);
 // For payment verification from RazorPay
 router.post('/verifyPayment', pay.verifyPayment);
 
 // Washerman routes
+router.post('/washerman/wing/fetchRecord', washerman.wingRecord);
+router.post('/washerman/upcomingDate', washerman.upcomingDate);
 
 export default router;
