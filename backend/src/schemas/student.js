a6498cc7662076854dd43f41ceeab8f6a8f8aeb8
its-me-yps <yps8181@gmail.com>
2024-03-17 14:01:26 +0530

    receipt Schema

--- backend/src/schemas/student.js
+++ backend/src/schemas/student.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
+const receiptSchema = new mongoose.Schema({
+    orderId: { type: String, required: true },
+    paymentId: { type: String, required: true },
+    amount: { type: Number, required: true },
+    currency: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now }
+});
+
 const studentSchema = new mongoose.Schema({
     roll: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
@@ -9,14 +17,15 @@ const studentSchema = new mongoose.Schema({
     wing: { type: String, required: true },
     // SHA256 of Password
     passHash: { type: String, required: true },
-    washermanId: { type: mongoose.Schema.Types.ObjectId, ref: 'Washerman' },
-    record: [{
-      date: { type: Date, required: true },
-      // Array of pair(type of cloth and number of units) in clothes
-      clothes: [{ type: { type: String, required: true }, quantity: { type: Number, required: true } }],
+    washerman: { type: mongoose.Schema.Types.ObjectId, ref: 'Washerman' },
+    records: [{
+        date: { type: Date, required: true },
+        // Array of pair(type of cloth and number of units) in clothes
+        clothes: [{ type: { type: String, required: true }, quantity: { type: Number, required: true } }],
     }],
     // last date whose dues were cleared, charges for all clothes after that are added in dueAmount
     lastCleared : { type: Date },
+    receipts: [receiptSchema], 
     dueAmount: { type: Number, required: true }
 }, { timestamps: true });
 
