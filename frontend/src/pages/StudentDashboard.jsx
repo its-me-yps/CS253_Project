1b1e8c20da7ca16c216310dcb88a73de11a7e335
vishalkmr22 <161647469+vishalkmr22@users.noreply.github.com>
2024-03-23 11:28:17 -0700

    Update StudentDashboard.jsx

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -32,7 +32,7 @@ function StudentDashboard(){
 
     return(
         <>
-        <div className="flex items-center justify-center flex-column p-10 max-w-xl mx-auto" style={{ backgroundColor: '#8CB9BD' }}>
+        <div className="flex items-center justify-center flex-column p-10 max-w-xl mx-auto" style={{ backgroundImage: 'linear-gradient(to bottom, #b1dfdfef, hsl(0, 35%, 85%))' }}>
             <div className="form">
        <CalendarTop2 user={user} onLogout={onLogout}/>
        <StudentCalendar/>
