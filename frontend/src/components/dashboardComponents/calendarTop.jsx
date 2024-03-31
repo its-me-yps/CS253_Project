9c4e454c7568c07e151dc184047eddd783d15377
AbhishekIITK-Y22 <136859559+AbhishekIITK-Y22@users.noreply.github.com>
2024-03-31 15:52:58 +0530

    Update calendarTop.jsx

--- frontend/src/components/dashboardComponents/calendarTop.jsx
+++ frontend/src/components/dashboardComponents/calendarTop.jsx
@@ -13,16 +13,16 @@ const handleGoBack = () => {
         <Button variant="contained" startIcon={<ArrowBackIcon />} onClick={handleGoBack}>Back</Button>
        
       </div>
-      <div className="flex flex-row">
-        <div className="border rounded-full ">
-          <img src="/karl-magnuson-85J99sGggnw-unsplash.jpeg" alt="User Profile" />
+      <div className="user-profile-container">
+        <div className="profile-pic ">
+          <img src="karl-magnuson-85J99sGggnw-unsplash.jpg" alt="User Profile" />
         </div>
         <div className="pl-2">
           <p><strong>Name:</strong> {user.name}</p>
           {user.email && <p><strong>email :</strong> {user.email}</p>}
           {user.contact && <p><strong>mobile no :</strong> {user.contact}</p>}
-          <p><strong>hall:</strong> {user.hall}</p>
-          <p><strong>wing:</strong> {user.wing}</p>
+          <p><strong>hall:</strong> {hall}</p>
+          <p><strong>wing:</strong> {wing}</p>
           <Button variant="contained" onClick={onLogout}>Logout</Button>
         </div>
       </div>
