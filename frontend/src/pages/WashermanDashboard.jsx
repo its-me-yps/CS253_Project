197f8b3c906e2369f2a5443d901d0936c508c38d
vishalkmr22 <161647469+vishalkmr22@users.noreply.github.com>
2024-04-03 17:29:55 -0700

    Update WashermanDashboard.jsx

--- frontend/src/pages/WashermanDashboard.jsx
+++ frontend/src/pages/WashermanDashboard.jsx
@@ -1,36 +1,42 @@
-import React from "react";
+import CalendarApp from "../components/dashboardComponents/calendarApp";
+import CalendarTop from "../components/dashboardComponents/calendarTop";
+import Notification from "../components/dashboardComponents/notification";
+import Footer from "../components/dashboardComponents/footerwashdash";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import CalendarTop2 from "../components/dashboardComponents/calendarTop2";
-import StudentCalendar from "../components/dashboardComponents/studentcalender";
-import { useNavigate } from 'react-router-dom';
 
-function StudentDashboard(){
-   const navigate = useNavigate();
-   
-   const infoCookie = Cookies.get('info');
-   const user = infoCookie ? JSON.parse(decodeURIComponent(infoCookie)) : null;
+function WashermanDashboard() {
+    const navigate = useNavigate();
+
+    const infoCookie = Cookies.get('info');
+    const user = infoCookie ? JSON.parse(decodeURIComponent(infoCookie)) : null;
 
     function onLogout() {
         Cookies.remove('info');
-        const response = fetch(`${process.env.REACT_APP_BACKEND_URL}/session/logout`, {
+        const response = fetch(`${process.env.REACT_APP_BACKEND_URL}/session/washerman/logout`, {
             method: "GET",
             credentials: "include",
         });
         navigate("/");
     }
+    const selectedHall=Cookies.get('selectedHall');
+    const selectedWing =Cookies.get('selectedWing');
 
-    return(
+    return (
         <>
-        <div className="flex items-center justify-center flex-column p-10 max-w-xl mx-auto" style={{ backgroundImage: 'linear-gradient(to bottom, #b1dfdfef, hsl(0, 35%, 85%))' }}>
-            <div className="form">
-       <CalendarTop2 user={user} onLogout={onLogout}/>
-       <StudentCalendar/>
-       </div>
-       </div>
-       </>
-
+            <div className="flex items-center justify-center flex-column p-10 max-w-xl mx-auto"style={{ backgroundImage: 'linear-gradient(to bottom, #b1dfdfef, hsl(0, 35%, 85%))' }}>
+                <div className="form">
+                    {/* Pass selected hall and wing to CalendarTop component */}
+                    <CalendarTop user={user} onLogout={onLogout} hall={selectedHall} wing={selectedWing} />
+                    <CalendarApp />
+                    <Notification />
+                    <Footer />
+                </div>
+            </div>
+        </>
     );
 }
 
-export default StudentDashboard;
+export default WashermanDashboard;
+
 
