6db7a203f0feec5957077f48e8d9626fb6d765aa
vishalkmr22 <vishalkmr22@iitk.ac.in>
2024-04-05 17:06:17 -0700

    fixed bug

--- frontend/src/pages/SummaryPage.jsx
+++ frontend/src/pages/SummaryPage.jsx
@@ -53,9 +53,9 @@ if(error)
 return (
   
       
-    <div className="summary-container">
+    <div>
       <Button variant="contained" onClick={backButton} className="bg-blue-500 text-white px-4 py-2 mt-4 text-lg  rounded-full justify-start font-extrabold" >
-        back
+        Back
       </Button>
       <h1><strong>Summary Page</strong></h1>
       {loading ? (
