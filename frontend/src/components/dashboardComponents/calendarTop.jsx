3e99cbf4687ac3c92e1d659fc09b8405ecd45fb9
vishalkmr22 <vishalkmr22@iitk.ac.in>
2024-04-06 06:41:05 -0700

    enabled back button to render to washerman selection page

--- frontend/src/components/dashboardComponents/calendarTop.jsx
+++ frontend/src/components/dashboardComponents/calendarTop.jsx
@@ -1,20 +1,24 @@
+import React from 'react';
 import Button from '@mui/material/Button'; 
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+
 import "./calendarTop.css";
 
-const CalendarTop = ({ user, onLogout,hall,wing }) => {
-const handleGoBack = () => {
-    console.log("Go back clicked");
-};
+const CalendarTop = ({ user, onLogout, hall, wing }) => {
+  const navigate = useNavigate(); // Move inside the component
+
+  const handleGoBack = () => {
+    navigate("/WashermanSelection");
+  };
 
   return (
     <div className='top'>
       <div className="p-4">
-        <Button variant="contained" disabled startIcon={<ArrowBackIcon />} onClick={handleGoBack} >Back</Button>
-       
+        <Button variant="contained" startIcon={<ArrowBackIcon />} onClick={handleGoBack}>Back</Button>
       </div>
       <div className="user-profile-container">
-        <div className="profile-pic ">
+        <div className="profile-pic">
           <img src="karl-magnuson-85J99sGggnw-unsplash.jpg" alt="User Profile" />
         </div>
         <div className="pl-2">
@@ -26,7 +30,7 @@ const handleGoBack = () => {
           <Button variant="contained" onClick={onLogout}>Logout</Button>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
